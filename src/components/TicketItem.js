b14ea80aaa47a72c3e42f08de030799a3bcdd9e4
Abhishek Kokate <Abhishekkokate8@gmail.com>
2024-10-12 23:37:15 +0530

    Edit and Delete btn added

--- src/components/TicketItem.js
+++ src/components/TicketItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../styles/TicketItem.css";
+import { ReactComponent as PencilIcon } from "../assets/pencil-solid.svg";
+import { ReactComponent as DeleteIcon } from "../assets/trash-can-solid.svg";
 
 const sliceDetailsString = (str) => {
   // If the string length is greater than allowed length, then this function will cut the string and add "..." at the end of the string to prevent overflowing or big cards in case of bigger string
@@ -11,12 +13,33 @@ const sliceDetailsString = (str) => {
 };
 
 function TicketItem({ ticket }) {
+  const performAction = (e, action) => {
+    e.preventDefault();
+    if (action === "edit") {
+    }
+  };
   return (
     <div className="item-container">
       <div className="item-card">
         <span className="item-id">
           <b>Id:</b> {ticket?.id}
         </span>
+        <a
+          href="/"
+          className="action-btn btn-edit icon"
+          onClick={(e) => performAction(e, "edit")}
+          title="Edit"
+        >
+          <PencilIcon />
+        </a>
+        <a
+          href="/"
+          className="action-btn btn-delete icon"
+          onClick={(e) => performAction(e, "edit")}
+          title="Delete"
+        >
+          <DeleteIcon />
+        </a>
         <div className="item-details">
           <p>
             <b>Details:</b> {sliceDetailsString(ticket?.todo || "")}
